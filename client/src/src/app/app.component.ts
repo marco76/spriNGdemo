a179f603220f5055a63523853352bf6c993e31c9
marco76 <moltenma@gmail.com>
2017-08-17 00:02:09 +0200

    menu

--- client/src/src/app/app.component.ts
+++ client/src/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, HostListener, OnInit, Output, ViewChild} from '@angular/core';
+import {MdSidenav} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  private bigScreen: boolean;
+  private openedSidenav: boolean;
+
+  @Output() toggleSidenav = new EventEmitter();
+  @ViewChild('sidenav') sidenav: MdSidenav;
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+       this.configureSideNav()
+  }
+
   ngOnInit() {
+    this.configureSideNav();
+  }
+
+  configureSideNav() {
+    this.bigScreen = window.innerWidth > 800;
+    this.openedSidenav = this.bigScreen;
   }
 }
