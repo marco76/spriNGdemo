569a6a6b63d905b422f39f8b9dee32666462b03a
marco76 <moltenma@gmail.com>
2017-08-18 20:54:46 +0200

    fix openshift

--- client/src/src/environments/environment.prod.ts
+++ client/src/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   BACKEND_URL : window.location.protocol + '//' + window.location.hostname + ':' + window.location.port,
-  WS_BACKEND_URL : 'ws://' + window.location.hostname + ':' + window.location.port
+  WS_BACKEND_URL : 'ws://' + window.location.hostname + ':' + window.location.port,
+  DOCUMENT_VALUES: {'BACKEND_URL': window.location.protocol + '//' + window.location.hostname + ':' + 8080}
 
 };
